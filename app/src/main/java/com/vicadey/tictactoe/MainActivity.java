63038332d94a4e3ae92ecf9934ca589522e4c064
kunno <victoradey@gmail.com>
2016-10-17 23:07:21 -0700

    Version 1.1

--- app/src/main/java/com/vicadey/tictactoe/MainActivity.java
+++ app/src/main/java/com/vicadey/tictactoe/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +16,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MobileAds.initialize(getApplicationContext(), "ca-app-pub-7080727738421282~5574841457");
+
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
 
         //setting image view for title and button
         ImageView title = (ImageView) findViewById(R.id.title);
